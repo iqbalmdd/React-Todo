7f737765ad94eb19b66669ccce368ed689e2ac69
iqbalmdd <iqbalmuhammad1290@gmail.com>
2024-07-04 00:28:06 +0700

    Skeleton

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./TaskForm.css"
+
+const TaskForm = () => {
+  return (
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enter your task"
+        />
+        {/* Button Tag */}
+        <div className="task_form_bottom_line">
+          <button className="tag">HTML</button>
+          <button className="tag">CSS</button>
+          <button className="tag">JavaScript</button>
+          <button className="tag">React</button>
+
+          {/* DropDown */}
+          <select className="task_status">
+            <option value="todo">To Do</option>
+            <option value="doing">Doing</option>
+            <option value="done">Done</option>
+          </select>
+          {/* Submit Button */}
+          <button className="task_submit" type="submit">
+            + Add Task
+          </button>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskForm;
